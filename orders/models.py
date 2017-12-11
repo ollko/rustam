49b329418e90767017934b1b66f6488007350eb7
Olga <korotkaya.olga@yandex.ru>
2017-12-11 13:14:30 +0300

    Change path to order.pdf on disk

--- orders/models.py
+++ orders/models.py
@@ -84,9 +84,8 @@ def post_save_receiver_order_model(sender, instance, created, **kwargs):
 		
 		# writing pdf to disk:
 		order_id = instance.id
-		# filename = os.path.join(BASE_DIR,"orders/tmp/Order_%s.pdf")%(order_id)
-		filename = '/home/u67523/posuda-pls.ru/rustam2/My_order.pdf'
-		print 'filename=',filename
+		filename = os.path.join(BASE_DIR,"orders/tmp/Order_%s.pdf")%(order_id)
+
 		context = {
 				'path_to_fonts':os.path.join(BASE_DIR,'static/fonts'),
 				'order': instance,
