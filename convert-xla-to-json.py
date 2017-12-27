72df952713c3c7c50400e2435add49d5b5781b00
Olga <korotkaya.olga@yandex.ru>
2017-12-27 20:26:48 +0300

    add new shop.json with new fields, add new fields to product card templates

--- convert-xla-to-json.py
+++ convert-xla-to-json.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import xlrd
 from collections import OrderedDict
 import json
@@ -5,7 +6,7 @@ import json
 import datetime
 import pytz
 
-wb = xlrd.open_workbook('list-of-products.xls')
+wb = xlrd.open_workbook('list-of-products_2.xls')
 sh = wb.sheet_by_index(1)
 
 products_list = []
@@ -18,19 +19,22 @@ for rownum in range(1, sh.nrows):
 	product['model']  = 'shop.product'
 	product['pk']     = rownum
 	product['fields'] = OrderedDict()
-	product['fields']['category'] = row_values[14]
+	product['fields']['category'] = row_values[19]
 
-	product['fields']['name'] = row_values[0]
+	product['fields']['name'] = row_values[13]
 	product['fields']['slug'] = 'page-slug'
 	product['fields']['image'] = ''
 	product['fields']['article'] = "0000000000000"
-	product['fields']['price'] = row_values[12]
+	product['fields']['price'] = row_values[14]
 	product['fields']['available'] = True
 	# С учетом временной зоны по Москве
 	product['fields']['created'] = str(datetime.datetime.now(tz))
 
 	product['fields']['updated'] = str(datetime.datetime.now(tz))
 
+	product['fields']['manufacture'] = row_values[15]
+	product['fields']['packaging'] = row_values[16]
+	product['fields']['typeprice'] = row_values[17]
 
 
 	products_list.append(product)
