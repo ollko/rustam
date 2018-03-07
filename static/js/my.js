8aa8ffae147c05792b93a1e71f2be8e4e76b3fe9
Olga <korotkaya.olga@yandex.ru>
2018-03-07 15:04:06 +0300

    product list like grid on mobilephone

--- static/js/my.js
+++ static/js/my.js
@@ -57,6 +57,15 @@ for (var i=1; i<=icons.length; i++){
 }
 
 hideAllViews();
-$(".products-view-2").show();
-$(".icon-2").addClass("dark");
+var doc_w = $(document).width();
+console.log(doc_w);
+if (doc_w<=500){
+	$(".products-view-1").show();
+	$(".icon-1").addClass("dark");
+}
+else{
+	$(".products-view-2").show();
+	$(".icon-2").addClass("dark");
+}
+
 // ---/ Переключение grid/list of Products
