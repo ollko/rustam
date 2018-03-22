e3e8b794f40eb1acf88aac62c5ad27840ab3a464
Olga <korotkaya.olga@yandex.ru>
2018-03-22 11:48:18 +0300

    Fix error whith two cart

--- static/js/my.js
+++ static/js/my.js
@@ -31,40 +31,40 @@ var productViews = $(".products-view");
 var icons = $(".icon");
 
 function hideAllViews(){
-	for (var i=0; i<productViews.length; i++){
+	for (var i = 0; i < productViews.length; i++){
 		$(productViews[i]).hide();
 	}
 }
 
 function bindIconToView(idNumber){
-	$(".icon-"+idNumber).click(function(){
+	$(".icon-" + idNumber).click(function(){
 		hideAllViews();
-		$(".products-view-"+idNumber).show();
+		$(".products-view-" + idNumber).show();
 
-		for (var i=1; i<=icons.length; i++){
-			if (i==idNumber) {
-				$(".icon-"+i).addClass("dark");
+		for (var i = 1; i <= icons.length; i++){
+			if (i == idNumber) {
+				$(".icon-" + i).addClass("dark");
 			}
 			else {
-				$(".icon-"+i).removeClass("dark");
+				$(".icon-" + i).removeClass("dark");
 			}
 		}
 	})
 }
 
-for (var i=1; i<=icons.length; i++){
+for (var i = 1; i <= icons.length; i++){
 	bindIconToView(i);
 }
 
 hideAllViews();
 var doc_w = $(document).width();
-console.log(doc_w);
-if (doc_w<=500){
+// console.log(doc_w);
+if (doc_w <= 500){
 	$(".products-view-1").show();
 	$(".icon-1").addClass("dark");
 }
 else{
-	$(".products-view-2").show();
-	$(".icon-2").addClass("dark");
+	// $(".products-view-2").show();
+	// $(".icon-2").addClass("dark");
 }
 // ---/ Переключение grid/list of Products
