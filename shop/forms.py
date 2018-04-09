2d3a9e223c4fe6aaec1fd348e6f4f2eb82000828
Olga <korotkaya.olga@yandex.ru>
2018-04-09 11:49:24 +0300

    Добавила фильтр категорий при добавлении товара из внешнего интерфейса

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product
+from .models import Product, Category
 
 class ProductCreateForm(forms.ModelForm):
     """A form for creating new Products."""
@@ -8,3 +8,7 @@ class ProductCreateForm(forms.ModelForm):
         model = Product
         fields = '__all__'
         widgets = {'name': forms.Textarea(attrs={'rows':3, 'cols':20})}
+
+    def __init__(self, *args, **kwargs):
+        super(ProductCreateForm, self).__init__(*args, **kwargs)
+        self.fields['category'].queryset = Category.objects.filter(id__in = [category.id for category in Category.objects.all() if category.is_leaf_node()])
